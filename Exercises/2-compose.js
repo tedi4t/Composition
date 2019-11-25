db76a68826320216f490f17da302fd2418d5c9c5
tedi4t <44302526+tedi4t@users.noreply.github.com>
2019-11-25 16:29:07 +0200

    Update 2-compose.js

--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  fns.reverse();
+  const f = x => {
+    try {
+      return fns.reduce((acc, val) => val(acc), x);
+    } catch (e) {
+      f.emit('error', e);
+    }
+  };
+  f.subs = {};
+  f.on = (e, fn, s = f.subs[e]) => (s ? s.push(fn) : f.subs[e] = [fn]);
+  f.emit = (even, data, s = f.subs[even]) => s.forEach(g => g(data));
+  return f;
+};
 
 module.exports = { compose };
