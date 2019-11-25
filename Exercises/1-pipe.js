f2613df8e41348d78e44894b63e74e40d9b99011
tedi4t <44302526+tedi4t@users.noreply.github.com>
2019-11-25 16:28:21 +0200

    Update 1-pipe.js

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,9 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  if (!fns.every(fn => typeof fn === 'function'))
+    throw new Error('Not all arguments are functions');
+  return x => fns.reduce((value, fn) => fn(value), x);
+};
 
 module.exports = { pipe };
